51d9b8914f5728afbcd017bf0c1a1a5fbb33b99f
Sanjeev Kumar <aisanjeevit@gmail.com>
2021-10-01 04:18:51 +0530

    first commit

---
+++ eventbiz/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 8-1, 1, 8)}); // year, month, date, hour
+        });
+});		
+
